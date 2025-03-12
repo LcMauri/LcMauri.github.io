a82199303c436b8ab5785665bb8951100edcd24f
LcMauri <94177490+LcMauri@users.noreply.github.com>
2025-03-12 19:46:42 +0100

    Update script.js

--- web-client/ch62/script.js
+++ web-client/ch62/script.js
@@ -1,1 +1,12 @@
 
+document.addEventListener("DOMContentLoaded", function () {
+    function showPopup() {
+        alert("Ceci est une alerte qui s'affiche toutes les 10 secondes !");
+    }
+    
+    // Afficher une première alerte au démarrage
+    showPopup();
+    
+    // Déclencher l'alerte toutes les 10 secondes
+    setInterval(showPopup, 10000);
+});
